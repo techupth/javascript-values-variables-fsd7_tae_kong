9a2c16e0d35853b9da1ac96a3a04ec0e28bcab0a
Kong Thiraphat <thiraphat.cto@gmail.com>
2024-09-30 13:44:50 +0700

    feat: add answer to exercise

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,17 @@
 //Exercise #1: Variable Creation
 //Start coding here
 
+let firstname = "Karter"
+let lastname  = "Anderson"
+let address  = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai"
+
+const restaurantName = "Chef's Table by Chef James"
+const netPrice = 15000
+const nowOpenRestaurants = 1500
+
 //Exercise #2: Variable Reassignment
 //Start coding here
+
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641"
+netPrice = 5000
+nowOpenRestaurants = 2500
--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,12 @@
 // Exercise #3: Celsius to Fahrenheit
 let celsius = 30;
+celsius = (celsius * 1.8) +32
+
+console.log(celsius)
 
 // Start coding here;
 
 let fahrenheit;
+fahrenheit = celsius
+
+console.log(fahrenheit)
